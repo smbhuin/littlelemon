978c916e3547d732fa05bc6086f4965e9905c081
Soumen Bhuin <smbhuin@gmail.com>
2025-10-29 10:08:04 +0530

    first commit

---
+++ littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Soumen Bhuin on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
